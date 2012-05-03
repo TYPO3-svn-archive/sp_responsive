2dc5da264a232d26054e47a47ec023ab24cce87b
speedprogs_de <speedprogs_de@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-05-03 20:04:45 +0000

    version 1.0.1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sp_responsive/trunk@61421 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "sp_responsive".
 #
-# Auto generated 29-04-2012 19:20
+# Auto generated 03-05-2012 21:19
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,13 +15,11 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Responsive image gallery and carousel',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '1.0.0',
+	'version' => '1.0.1',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
-	'TYPO3_version' => '',
-	'PHP_version' => '',
 	'module' => '',
 	'state' => 'beta',
 	'uploadfolder' => 0,
@@ -34,6 +32,15 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => 'Speedprogs.de',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:26:{s:12:"ext_icon.gif";s:4:"a1bd";s:14:"ext_tables.php";s:4:"94c1";s:14:"ext_tables.sql";s:4:"5eba";s:38:"Configuration/TypoScript/constants.txt";s:4:"378a";s:34:"Configuration/TypoScript/setup.txt";s:4:"fb2b";s:40:"Resources/Private/Language/locallang.xml";s:4:"5c60";s:52:"Resources/Public/Contrib/Elastislide/elastislide.css";s:4:"f35f";s:57:"Resources/Public/Contrib/Elastislide/jquery.easing.1.3.js";s:4:"6516";s:58:"Resources/Public/Contrib/Elastislide/jquery.elastislide.js";s:4:"d4e6";s:50:"Resources/Public/Contrib/Fittext/jquery.fittext.js";s:4:"3b63";s:47:"Resources/Public/Contrib/Flexslider/LICENSE.txt";s:4:"0fea";s:46:"Resources/Public/Contrib/Flexslider/README.txt";s:4:"e107";s:50:"Resources/Public/Contrib/Flexslider/flexslider.css";s:4:"b040";s:60:"Resources/Public/Contrib/Flexslider/jquery.flexslider-min.js";s:4:"3ec3";s:56:"Resources/Public/Contrib/Flexslider/jquery.flexslider.js";s:4:"fefc";s:60:"Resources/Public/Contrib/Flexslider/theme/bg_control_nav.png";s:4:"5ccb";s:62:"Resources/Public/Contrib/Flexslider/theme/bg_direction_nav.png";s:4:"b35b";s:51:"Resources/Public/Contrib/jQuery/jquery-1.7.2.min.js";s:4:"b8d6";s:38:"Resources/Public/Images/Navigation.png";s:4:"4262";s:41:"Resources/Public/Images/Icon/Carousel.gif";s:4:"a1bd";s:44:"Resources/Public/Images/Icon/Imageslider.gif";s:4:"4bb0";s:43:"Resources/Public/Images/Wizard/Carousel.gif";s:4:"0c6d";s:46:"Resources/Public/Images/Wizard/Imageslider.gif";s:4:"d313";s:38:"Resources/Public/Javascript/Default.js";s:4:"93dc";s:39:"Resources/Public/Stylesheet/Default.css";s:4:"bcee";s:14:"doc/manual.sxw";s:4:"3945";}',
 );
 
 ?>
